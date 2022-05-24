171338300940629c9b8eed15942cfc2bef3eca70
ashwinkanchana-deloitte <101543674+ashwinkanchana-deloitte@users.noreply.github.com>
2022-05-24 23:13:26 +0530

    Room design

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,67 +1,31 @@
-import React, { useState } from 'react'
-import RoomLayout from '../RoomLayout'
-import './style.css'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Join from '../Join';
+import React, { useState } from "react";
+import RoomLayout from "../RoomLayout";
+import "./style.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Join from "../Join";
 import { getsocketIoInstance } from "../../utils/socketio-client";
-
+import {
+  styled,
+  useTheme,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
 let memberElems;
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 export default function App() {
-    const [verified, setVerified] = useState(false);
-    const [modalOpen, setModalOpen] = useState(false);
+  const [verified, setVerified] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const toggleMembers = () => {
-        if (!modalOpen) {
-            const mJson = sessionStorage.getItem('members');
-            if (mJson) {
-                const members = JSON.parse(mJson);
-                memberElems = Object.keys(members).map(key =>
-                    <span key={key}> - {members[key]}</span>
-                );
-            }
-            setModalOpen(true);
-        } else {
-            setModalOpen(false);
-        }
-
-    }
-
-    const exitWorkspace = () => {
-        // TODO: have a dialog or sth
-        const roomName = sessionStorage.getItem('roomName');
-        const displayName = sessionStorage.getItem('displayName');
-        const socketIo = getsocketIoInstance(roomName, displayName, 'App');
-        socketIo.emit('leave-room', { roomName, userName: displayName });
-        setVerified(false);
-    }
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>Collab Space</p>
-                {verified && <div className="room-details-cont">
-                    <div className="room-details">
-                        <span>Room Name: {sessionStorage.getItem('roomName')}</span>
-                        <span>UserName: {sessionStorage.getItem('displayName')}</span>
-                        <span></span>
-                    </div>
-                    <i className="fa fa-users room-icon" aria-hidden="true" data-tip="Members" onClick={toggleMembers}></i>
-                    <i className="fas fa-times-circle room-icon" data-tip="Exit Workspace" onClick={exitWorkspace}></i>
-                </div>}
-            </header>
-            <RoomLayout />
-            {/* {!verified && <Join setVerified={setVerified} />}
-            {verified && <RoomLayout />}
-            {modalOpen && <div className="members-modal-content">
-                <i class="fa fa-times mbrs-mdl-close" aria-hidden="true" onClick={() => setModalOpen(false)}></i>
-                <h6>These users have access to this Workspace</h6>
-                {memberElems}
-            </div> */}
-            
-
-            <ToastContainer limit={3} />
-        </div>
-    );
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <RoomLayout />
+    </ThemeProvider>
+  );
 }
--- client/src/components/RoomWorkArea.js
+++ client/src/components/RoomWorkArea.js
@@ -1,86 +1,268 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import Code from './Code'
-import Whiteboard from './Whiteboard'
-import Whiteboardsocket from './Whiteboardsocket'
-import Grid from '@mui/material/Grid';
-import CodeIcon from '@mui/icons-material/Code';
-import GestureIcon from '@mui/icons-material/Gesture';
+// import React from 'react'
+// import PropTypes from 'prop-types';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+// import Box from '@mui/material/Box';
+import React, { useState } from "react";
+import {
+  Box,
+  Toolbar,
+  List,
+  Typography,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+} from "@mui/material";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import { Code, Videocam, Gesture } from "@mui/icons-material";
+import {
+  styled,
+  useTheme,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
+
+import CodeComponent from "./Code";
+import Whiteboard from "./Whiteboard";
+import Whiteboardsocket from "./Whiteboardsocket";
+import Grid from "@mui/material/Grid";
+
+// function TabPanel(props) {
+//     const { children, value, index, ...other } = props;
+
+//     return (
+//         <div
+//             role="tabpanel"
+//             hidden={value !== index}
+//             id={`simple-tabpanel-${index}`}
+//             aria-labelledby={`simple-tab-${index}`}
+//             {...other}
+//         >
+//             {value === index && (
+//                 <Box>
+//                     {children}
+//                 </Box>
+//             )}
+//         </div>
+//     );
+// }
+
+// TabPanel.propTypes = {
+//     children: PropTypes.node,
+//     index: PropTypes.number.isRequired,
+//     value: PropTypes.number.isRequired,
+// };
+
+// function a11yProps(index) {
+//     return {
+//         id: `simple-tab-${index}`,
+//         'aria-controls': `simple-tabpanel-${index}`,
+//     };
+// }
+
+// export default function BasicTabs() {
+//     const [value, setValue] = React.useState(0);
+
+//     const handleChange = (event, newValue) => {
+//         setValue(newValue);
+//     };
+
+//     return (
+//         <>
+//             <Grid container >
+//                 <Grid item xs={1}>
+//                     <Box
+//                         sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: '100%' }}>
+//                         <Tabs value={value}
+
+//                             orientation="vertical"
+//                             onChange={handleChange}
+//                             sx={{ borderRight: 1, borderColor: 'divider' }}>
+//                             <Tab icon={<CodeIcon fontSize="small" />} label="Code" aria-label="phone" {...a11yProps(0)} />
+//                             <Tab icon={<GestureIcon fontSize="small" />} label="Scribble" aria-label="favorite" {...a11yProps(1)} />
+//                         </Tabs>
+//                     </Box>
+//                 </Grid>
+//                 <Grid item xs={1} sm={11} md={11} lg={11} xl={11}>
+//                     <TabPanel value={value} index={0}>
+//                         <Code />
+//                     </TabPanel>
+//                     <TabPanel value={value} index={1}>
+//                         {/* <Whiteboard /> */}
+//                         <Whiteboardsocket/>
+//                     </TabPanel>
+//                 </Grid>
+
+//             </Grid>
+
+//         </>
+//     );
+// }
+
+
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box>
-                    {children}
-                </Box>
-            )}
-        </div>
-    );
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 1 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-};
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
 
-function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
 
-export default function BasicTabs() {
-    const [value, setValue] = React.useState(0);
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+}));
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
 
-    return (
-        <>
-            <Grid container >
-                <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
-                    <Box
-                        sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: '100%' }}>
-                        <Tabs value={value}
+  ...closedMixin(theme),
+  "& .MuiDrawer-paper": closedMixin(theme),
+}));
 
-                            orientation="vertical"
-                            onChange={handleChange}
-                            sx={{ borderRight: 1, borderColor: 'divider' }}>
-                            <Tab icon={<CodeIcon fontSize="small" />} label="Code" aria-label="phone" {...a11yProps(0)} />
-                            <Tab icon={<GestureIcon fontSize="small" />} label="Scribble" aria-label="favorite" {...a11yProps(1)} />
-                        </Tabs>
-                    </Box>
-                </Grid>
-                <Grid item xs={1} sm={11} md={11} lg={11} xl={11}>
-                    <TabPanel value={value} index={0}>
-                        <Code />
-                    </TabPanel>
-                    <TabPanel value={value} index={1}>
-                        {/* <Whiteboard /> */}
-                        <Whiteboardsocket/>
-                    </TabPanel>
-                </Grid>
+export default function MiniDrawer() {
+  const theme = useTheme();
 
-            </Grid>
+  const [tabValue, setTabValue] = useState(0);
 
 
+  return (
+    
+      <Box sx={{ display: "flex" }}>
+        <AppBar position="fixed" open={false}>
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Mini variant drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={false}>
+          <DrawerHeader/>
 
+          <List>
+            <ListItem
+              onClick={() => setTabValue(0)}
+              disablePadding
+              sx={{ display: "block" }}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Code />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
 
-        </>
-    );
+            <ListItem
+              onClick={() => setTabValue(1)}
+              disablePadding
+              sx={{ display: "block" }}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Videocam />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+            <ListItem
+              onClick={() => setTabValue(2)}
+              disablePadding
+              sx={{ display: "block" }}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Gesture />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <TabPanel value={tabValue} index={0}>
+            <CodeComponent />
+          </TabPanel>
+          <TabPanel value={tabValue} index={1}>
+            <Whiteboard />
+          </TabPanel>
+          <TabPanel value={tabValue} index={2}>
+            <Whiteboardsocket />
+          </TabPanel>
+        </Box>
+      </Box>
+  );
 }
